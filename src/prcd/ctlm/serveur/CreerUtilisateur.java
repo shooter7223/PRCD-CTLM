ac94667a15f0ba964c80682a1503bb74b5075501
thoma <thoma@DESKTOP-UV1QIJS>
2017-01-25 16:49:47 +0100

    creation client ok

--- src/prcd/ctlm/serveur/CreerUtilisateur.java
+++ src/prcd/ctlm/serveur/CreerUtilisateur.java
@@ -7,6 +7,7 @@ package prcd.ctlm.serveur;
 
 import java.io.*;
 import java.beans.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -16,30 +17,63 @@ public class CreerUtilisateur {
     
     public void NouveauUtilisateur(String id, String nom, String prenom, String mail, String pass) throws IOException
     {        
-        FileWriter  xml = new FileWriter ("Utilisateurs.xml", true);
-        xml.write("<utilisateur>\n");
+        BufferedReader br = new BufferedReader(new FileReader("Utilisateurs.xml"));
+        String line;
+        ArrayList<String> xml_save = new ArrayList<String>();
+        boolean doc_vide = true;
+        
+        while ((line = br.readLine()) != null) {
+                doc_vide = false;
+                System.out.println(line);
+                xml_save.add(line);
+               
+        }
+        
+        FileWriter  xml = new FileWriter ("Utilisateurs.xml");
+        
+        
+        
+        if(doc_vide)
+        {
+            xml.write("<root>\n");
+        
+        }else xml.write(xml_save.get(0)+"\n\n");
+       
+        
+        
+        xml.write("<utilisateur>");
         
         xml.write("<id>");
         xml.write(id);
-        xml.write("</id>\n");
+        xml.write("</id>");
         
         xml.write("<nom>");
         xml.write(nom);
-        xml.write("</nom>\n");
+        xml.write("</nom>");
         
         xml.write("<prenom>");
         xml.write(prenom);
-        xml.write("</prenom>\n");
+        xml.write("</prenom>");
         
         xml.write("<e-mail>");
         xml.write(mail);
-        xml.write("</e-mail>\n");
+        xml.write("</e-mail>");
         
         xml.write("<motDePasse>");
         xml.write(pass);
-        xml.write("</motDePasse>\n");
+        xml.write("</motDePasse>");
         
-        xml.write("</utilisateur>\n");
+        xml.write("</utilisateur>");
+        
+        if(doc_vide == false)
+        {
+            for(int j = 1;j < xml_save.size();j++)
+            {
+                xml.write(xml_save.get(j));
+            }
+        
+        }
+        else xml.write("</root>\n");
         
         xml.flush();
         xml.close();
