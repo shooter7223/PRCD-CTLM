8de72e3b2411e8e4d913a80e240c1a034f6fb8bc
esber <esber@172.18.210.189>
2017-01-28 17:11:32 +0100

    Mise à jour de l'apparence : il faut regarder les marges et les couleurs. Ajout des pages de base : achèvement, prise, attribution et suppression. Mise à jour des transitions : il reste des contrôleurs à créer.

--- src/prcd/ctlm/projet/IdentificationController.java
+++ src/prcd/ctlm/projet/IdentificationController.java
@@ -7,28 +7,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 
-
-/**
- *
+/*
  * @author Thomas Caspar and Thérésien Esberard
  */
+
 public class IdentificationController {
 
-    private CTLMProject ctlm = new CTLMProject();;
-    private MultieClient client = new MultieClient() ;
+    private CTLMProject ctlm = new CTLMProject();
+    private MultieClient client = new MultieClient();
     
     @FXML
     private TextField identifiantBox, mdpBox;
     
-    
     @FXML
     private void connection(ActionEvent e) throws IOException{
         
-        client.clientLog(identifiantBox.getText(), mdpBox.getText(), "n");
+        //client.clientLog(identifiantBox.getText(), mdpBox.getText(), "n");
         ctlm.goHome(e);
 
     }
-    
+        
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
--- src/prcd/ctlm/projet/ViewTasksController.java
+++ src/prcd/ctlm/projet/ViewTasksController.java
@@ -25,25 +25,11 @@ public class ViewTasksController {
     
     @FXML
     private void dummy(ActionEvent e) throws IOException{
-        //ctlm.showHome();
-        Stage stage; 
-        VBox dummy;
-     
-        // get reference to the button's stage to stay at the same page
-        stage = (Stage) ((Button) e.getSource()).getScene().getWindow();
-        
-        //load up OTHER FXML document
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(CTLMProject.class.getResource("Dummy.fxml"));
-        dummy = loader.load();
-        Scene scene = new Scene(dummy);
-        stage.setScene(scene);
-        stage.show();
+        ctlm.dummy(e);
     }
     
     @FXML
     private void task(ActionEvent e) throws IOException{
-        //ctlm.showHome();
         Stage stage; 
         VBox task;
      
