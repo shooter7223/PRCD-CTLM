c821471dd13be659bb0756a4e5cb436c48340354
esber <esber@172.18.210.189>
2017-01-29 16:51:58 +0100

    Mise à jour des pages de finition, suppression et prise.

---
+++ src/prcd/ctlm/projet/TakeTaskController.java
@@ -0,0 +1,30 @@
+package prcd.ctlm.projet;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+/**
+ * @author Thomas Caspar and Thérésien Esberard
+ */
+
+public class TakeTaskController {
+    CTLMProject ctlm = new CTLMProject();
+    
+    @FXML
+    private void goHome(ActionEvent e) throws IOException{
+        ctlm.goHome(e);
+    }
+    
+    @FXML
+    private void finish(ActionEvent e) throws IOException{
+        ctlm.goHome(e);
+    }
+    
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
