945a1cbfc1f97ed8dd216b8731cb55f53b6555f4
thoma <thoma@DESKTOP-UV1QIJS>
2017-01-30 17:03:16 +0100

    organisation

--- src/prcd/ctlm/Serveur.java
+++ src/prcd/ctlm/Serveur.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package prcd.ctlm.serveur;
+package prcd.ctlm;
 
 import java.io.*;
 import java.net.*;
+import prcd.ctlm.serveur.ServeurThread;
 
 public class Serveur {
  public static ServerSocket ss = null;
